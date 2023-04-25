733add8175f37febcd70202d5094cecdc422d620
johnpaulgrg <johnpaulgrg1996@gmail.com>
2023-04-25 11:50:14 +0530

    update

--- script.js
+++ script.js
@@ -1,160 +1,171 @@
-const predefinedLocations = [
-  // Gurugram Locations
+let map;
+let userMarker;
+const aedIcon = "aed-icon.png"; // Replace with your custom image URL
+
+const gurugramLocations = [
   {
     lat: 28.407945984784263,
     lng: 77.1165467236218,
     name: "AED 1",
+    status: "working",
     address: "Address 1",
-    status: "Working",
   },
   {
     lat: 28.40726040753197,
     lng: 77.11703320021923,
     name: "AED 2",
+    status: "working",
     address: "Address 2",
-    status: "Working",
   },
   {
     lat: 28.40625064816643,
     lng: 77.11638947005999,
     name: "AED 3",
+    status: "working",
     address: "Address 3",
-    status: "Working",
   },
   {
     lat: 28.40601472169394,
     lng: 77.11425443169855,
     name: "AED 4",
+    status: "working",
     address: "Address 4",
-    status: "Working",
   },
-  // Manipal Locations
   {
-    lat: 13.35209307245631,
-    lng: 74.79333562540025,
+    lat: 28.40857213663377,
+    lng: 77.11377163407913,
     name: "AED 5",
+    status: "not working",
     address: "Address 5",
-    status: "Not Working",
   },
   {
-    lat: 13.351743299193053,
-    lng: 74.79328923955707,
+    lat: 28.40857213663377,
+    lng: 77.1138038205871,
     name: "AED 6",
+    status: "not working",
     address: "Address 6",
-    status: "Not Working",
   },
   {
-    lat: 13.351367198344537,
-    lng: 74.79270555103045,
+    lat: 28.40924215026731,
+    lng: 77.11726923461093,
     name: "AED 7",
+    status: "not working",
     address: "Address 7",
-    status: "Not Working",
   },
   {
-    lat: 13.353664076063442,
-    lng: 74.78981318695902,
+    lat: 28.410383994722114,
+    lng: 77.11577792640871,
     name: "AED 8",
+    status: "not working",
     address: "Address 8",
-    status: "Not Working",
   },
 ];
 
-let map;
-let userLocation;
-let userMarker;
-
 function initMap() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        userLocation = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-        createMap(userLocation);
-        watchUserPosition();
-      },
-      () => {
-        handleLocationError(true);
-      }
-    );
-  } else {
-    handleLocationError(false);
-  }
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 28.4089, lng: 77.1162 },
+    zoom: 15,
+  });
+
+  createMarkers(gurugramLocations);
+  requestUserLocation();
 }
 
-function createMap(userLocation) {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: userLocation,
-    zoom: 14,
-  });
-
-  userMarker = new google.maps.Marker({
-    position: userLocation,
-    map: map,
-    title: "Your location",
-  });
-
+function createMarkers(locations) {
   const directionsService = new google.maps.DirectionsService();
-  const directionsRenderer = new google.maps.DirectionsRenderer();
-  directionsRenderer.setMap(map);
+  const directionsRenderer = new google.maps.DirectionsRenderer({ map: map });
 
-  const aedIcon = "aed-icon.png";
-
-  predefinedLocations.forEach((location) => {
+  locations.forEach((location) => {
     const marker = new google.maps.Marker({
       position: { lat: location.lat, lng: location.lng },
       map: map,
-      title: location.name,
-      label: location.name,
       icon: aedIcon,
+      title: location.name,
     });
 
-    marker.addListener("click", function () {
-      const request = {
-        origin: userLocation,
-        destination: marker.getPosition(),
-        travelMode: "DRIVING",
-      };
-      directionsService.route(request, function (result, status) {
-        if (status == "OK") {
-          directionsRenderer.setDirections(result);
-          const duration = result.routes[0].legs[0].duration.text;
-          const content = `<div><strong>${location.name}</strong><br>${location.address}<br>Status: ${location.status}<br>Estimated travel time: ${duration}</div>`;
-          const infoWindow = new google.maps.InfoWindow({
-            content: content,
-          });
-          infoWindow.open(map, marker);
-        }
-      });
+    const infoWindow = new google.maps.InfoWindow({
+      content: `<h3>${location.name}</h3><p>Status: ${location.status}</p><p>Address: ${location.address}</p>`,
+    });
+
+    marker.addListener("click", () => {
+      infoWindow.open(map, marker);
+
+      if (userMarker) {
+        const userLocation = userMarker.getPosition();
+        const destination = { lat: location.lat, lng: location.lng };
+
+        directionsService.route(
+          {
+            origin: userLocation,
+            destination: destination,
+            travelMode: google.maps.TravelMode.WALKING,
+          },
+          (result, status) => {
+            if (status === google.maps.DirectionsStatus.OK) {
+              directionsRenderer.setDirections(result);
+            } else {
+              console.error(`Directions request failed due to ${status}`);
+            }
+          }
+        );
+      }
     });
   });
 }
 
-function watchUserPosition() {
+function requestUserLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.watchPosition(
-      (position) => {
-        userLocation = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-        userMarker.setPosition(userLocation);
-        map.setCenter(userLocation);
-      },
-      () => {
-        handleLocationError(true);
-      }
-    );
+    navigator.geolocation.watchPosition(showUserLocation, handleError, {
+      enableHighAccuracy: true,
+      timeout: 10000,
+      maximumAge: 0,
+    });
   } else {
-    handleLocationError(false);
+    alert("Geolocation is not supported by your browser.");
   }
 }
+function showUserLocation(position) {
+  const userLocation = {
+    lat: position.coords.latitude,
+    lng: position.coords.longitude,
+  };
 
-function handleLocationError(browserHasGeolocation) {
-  alert(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
+  // Remove the existing user marker if it exists
+  if (userMarker) {
+    userMarker.setMap(null);
+  }
+
+  // Create a blue circle for the user's location
+  userMarker = new google.maps.Marker({
+    position: userLocation,
+    map: map,
+    icon: {
+      path: google.maps.SymbolPath.CIRCLE,
+      fillColor: "#4285F4",
+      fillOpacity: 1,
+      strokeColor: "white",
+      strokeWeight: 1,
+      scale: 7,
+    },
+    zIndex: google.maps.Marker.MAX_ZINDEX + 1,
+  });
+
+  map.panTo(userLocation);
+}
+
+function handleError(error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      alert("User denied the request for Geolocation.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      alert("Location information is unavailable.");
+      break;
+    case error.TIMEOUT:
+      alert("The request to get user location timed out.");
+      break;
+    case error.UNKNOWN_ERROR:
+      alert("An unknown error occurred.");
+      break;
+  }
 }
