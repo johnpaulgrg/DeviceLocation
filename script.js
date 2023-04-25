6341123eceed8e40c75caf548ae39ddce9ec0e79
johnpaulgrg <johnpaulgrg1996@gmail.com>
2023-04-25 13:08:11 +0530

    done

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let map;
 let userMarker;
-const aedIcon = "aed-icon.png"; // Replace with your custom image URL
+const aedIcon = "aed-icon.png";
 
-const gurugramLocations = [
+const allLocations = [
+  // Gurugram locations
   {
     lat: 28.407945984784263,
     lng: 77.1165467236218,
@@ -21,43 +22,73 @@ const gurugramLocations = [
     lat: 28.40625064816643,
     lng: 77.11638947005999,
     name: "AED 3",
-    status: "working",
+    status: "not working",
     address: "Address 3",
   },
   {
     lat: 28.40601472169394,
     lng: 77.11425443169855,
     name: "AED 4",
-    status: "working",
+    status: "not working",
     address: "Address 4",
   },
+  // Manipal locations
   {
-    lat: 28.40857213663377,
-    lng: 77.11377163407913,
+    lat: 13.35209307245631,
+    lng: 74.79333562540025,
     name: "AED 5",
-    status: "not working",
+    status: "working",
     address: "Address 5",
   },
   {
-    lat: 28.40857213663377,
-    lng: 77.1138038205871,
+    lat: 13.351743299193053,
+    lng: 74.79328923955707,
     name: "AED 6",
-    status: "not working",
+    status: "working",
     address: "Address 6",
   },
   {
-    lat: 28.40924215026731,
-    lng: 77.11726923461093,
+    lat: 13.351367198344537,
+    lng: 74.79270555103045,
     name: "AED 7",
     status: "not working",
     address: "Address 7",
   },
   {
-    lat: 28.410383994722114,
-    lng: 77.11577792640871,
+    lat: 13.353664076063442,
+    lng: 74.78981318695902,
     name: "AED 8",
     status: "not working",
     address: "Address 8",
+  },
+  // Kakkanad locations
+  {
+    lat: 10.0156779,
+    lng: 76.3378421,
+    name: "AED 9",
+    status: "working",
+    address: "Near Infopark Entrance, Infopark Rd, Kakkanad, Kochi, Kerala",
+  },
+  {
+    lat: 10.0201098,
+    lng: 76.3619053,
+    name: "AED 10",
+    status: "working",
+    address: "Rajagiri Valley, Kakkanad, Kochi, Kerala",
+  },
+  {
+    lat: 10.0047001,
+    lng: 76.3254134,
+    name: "AED 11",
+    status: "not working",
+    address: "Thrikkakara Temple, Thrikkakara North, Kakkanad, Kochi, Kerala",
+  },
+  {
+    lat: 9.9981663,
+    lng: 76.3080833,
+    name: "AED 12",
+    status: "not working",
+    address: "Kakkanad Civil Station, Kochi, Kerala",
   },
 ];
 
@@ -67,7 +98,7 @@ function initMap() {
     zoom: 15,
   });
 
-  createMarkers(gurugramLocations);
+  createMarkers(allLocations);
   requestUserLocation();
 }
 
@@ -83,13 +114,7 @@ function createMarkers(locations) {
       title: location.name,
     });
 
-    const infoWindow = new google.maps.InfoWindow({
-      content: `<h3>${location.name}</h3><p>Status: ${location.status}</p><p>Address: ${location.address}</p>`,
-    });
-
     marker.addListener("click", () => {
-      infoWindow.open(map, marker);
-
       if (userMarker) {
         const userLocation = userMarker.getPosition();
         const destination = { lat: location.lat, lng: location.lng };
@@ -98,11 +123,22 @@ function createMarkers(locations) {
           {
             origin: userLocation,
             destination: destination,
-            travelMode: google.maps.TravelMode.WALKING,
+            travelMode: google.maps.TravelMode.DRIVING,
           },
           (result, status) => {
             if (status === google.maps.DirectionsStatus.OK) {
               directionsRenderer.setDirections(result);
+
+              // Calculate time to reach the location
+              const duration = result.routes[0].legs[0].duration.text;
+
+              // Update InfoWindow content with time to reach
+              const infoWindowContent = `<h3>${location.name}</h3><p>Status: ${location.status}</p><p>Address: ${location.address}</p><p>Time to reach: ${duration}</p>`;
+              const infoWindow = new google.maps.InfoWindow({
+                content: infoWindowContent,
+              });
+
+              infoWindow.open(map, marker);
             } else {
               console.error(`Directions request failed due to ${status}`);
             }
